823952cba4cae5eb6006caa584fd3aacb1e1685f
Damon Zirkler <dzirkler@steelcase.com>
2017-02-02 19:06:51 -0800

    update volume path

--- plexpp.sh
+++ plexpp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -v /mnt/storage/media-files/tvshows:/data/tvshows dzirkler/comskip:latest /root/plexpp.sh "$1"
+docker run -it -v /mnt/storage/media-files:/data dzirkler/comskip:latest /root/plexpp.sh "$1"
